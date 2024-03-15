33fbe8ac8002d6614606892012eb7f1a70e4ff89
Ritwik <sritwik03@gmail.com>
2024-03-15 12:48:18 +0530

    task 10

--- app/server.go
+++ app/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/hex"
 	"flag"
 	"fmt"
 	"net"
@@ -174,6 +175,14 @@ func handleCommand(conn net.Conn) {
 		case "psync":
 			if rdb.role == "master" {
 				res = []byte("+FULLRESYNC 8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb 0\r\n")
+				conn.Write(res)
+				emptyRdbFileHex := "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"
+				emptyRdbFile, err := hex.DecodeString(emptyRdbFileHex)
+				if err != nil {
+					fmt.Println("Error decoding empty RDB file: ", err.Error())
+					return
+				}
+				res = []byte(fmt.Sprintf("$%d\r\n%s", len(emptyRdbFile), emptyRdbFile))
 			} else {
 				res = []byte("-ERR not a master\r\n")
 			}
