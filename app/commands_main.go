164dae0fd312eba9b4135a0c45d8d653dffeead6
LitoleNINJA <sritwik2@gmail.com>
2025-08-22 10:05:43 +0000

    codecrafters submit [skip ci]

--- app/commands_main.go
+++ app/commands_main.go
@@ -8,43 +8,6 @@ import (
 	"time"
 )
 
-// Redis response encoding functions
-func encodeSimpleString(s string) []byte {
-	return []byte(fmt.Sprintf("+%s\r\n", s))
-}
-
-func encodeBulkString(s string) []byte {
-	if s == "" {
-		return []byte("$-1\r\n") // null bulk string
-	}
-	return []byte(fmt.Sprintf("$%d\r\n%s\r\n", len(s), s))
-}
-
-func encodeInteger(i int64) []byte {
-	return []byte(fmt.Sprintf(":%d\r\n", i))
-}
-
-func encodeArray(elements []string) []byte {
-	result := fmt.Sprintf("*%d\r\n", len(elements))
-	for _, element := range elements {
-		result += fmt.Sprintf("$%d\r\n%s\r\n", len(element), element)
-	}
-	return []byte(result)
-}
-
-func encodeError(msg string) []byte {
-	return []byte(fmt.Sprintf("-ERR %s\r\n", msg))
-}
-
-func encodeNull() []byte {
-	return []byte("$-1\r\n")
-}
-
-type XReadEntry struct {
-	key   string
-	entry []redisStreamEntry
-}
-
 func handleCommand(cmd string, args []string, conn net.Conn, totalBytes int, rdb *redisDB) []byte {
 	connAddr := conn.RemoteAddr().String()
 	connState := rdb.getConnState(connAddr)
@@ -101,6 +64,8 @@ func handleCommand(cmd string, args []string, conn net.Conn, totalBytes int, rdb
 		response = handleLrangeCommand(args, rdb)
 	case "lpush":
 		response = handleLpushCommand(args, totalBytes, rdb)
+	case "llen":
+		response = handleLLenCommand(args, rdb)
 	default:
 		response = handleUnknownCommand(cmd, rdb)
 	}
@@ -245,26 +210,6 @@ func handleXRangeCommand(args []string, rdb *redisDB) []byte {
 	return buildStreamResponse(entries)
 }
 
-func parseStreamID(id string) (string, int) {
-	if strings.Contains(id, "-") {
-		parts := strings.Split(id, "-")
-		seq, _ := strconv.Atoi(parts[1])
-		return parts[0], seq
-	}
-	return id, 0
-}
-
-func buildStreamResponse(entries []redisStreamEntry) []byte {
-	resString := fmt.Sprintf("*%d\r\n", len(entries))
-	for _, entry := range entries {
-		resString += fmt.Sprintf("*2\r\n$%d\r\n%s\r\n", len(entry.id), entry.id)
-		for k, v := range entry.fields {
-			resString += fmt.Sprintf("*2\r\n$%d\r\n%s\r\n$%d\r\n%s\r\n", len(k), k, len(v), v)
-		}
-	}
-	return []byte(resString)
-}
-
 func handleXReadCommand(args []string, conn net.Conn, rdb *redisDB) []byte {
 	switch args[0] {
 	case "streams":
@@ -347,150 +292,6 @@ func handleUnknownCommand(cmd string, rdb *redisDB) []byte {
 		return encodeError("unknown command")
 	}
 	return []byte("")
-}
-
-func updateSlaveOffset(cmd string, totalBytes int, rdb *redisDB) {
-	if rdb.role == "slave" && handshakeComplete {
-		rdb.offset += totalBytes
-		fmt.Printf("\nCmd: %s,  Current Bytes: %d,  Bytes processed: %d\n", cmd, totalBytes, rdb.offset)
-	}
-}
-
-func validateStreamID(id string) string {
-	if id == "0-0" {
-		return "The ID specified in XADD must be greater than 0-0"
-	}
-	if id <= lastStreamID {
-		return "The ID specified in XADD is equal or smaller than the target stream top item"
-	}
-	return ""
-}
-
-func handleXRead(args []string, rdb *redisDB) []XReadEntry {
-	keyEndPos := findKeyEndPosition(args)
-	keys := args[:keyEndPos]
-	ids := args[keyEndPos:]
-
-	fmt.Println("Keys: ", keys, " IDs: ", ids)
-
-	entries := make([]XReadEntry, 0)
-	for i := 0; i < len(keys); i++ {
-		streamEntries := getStreamEntriesAfterID(rdb, keys[i], ids[i])
-		if len(streamEntries) > 0 {
-			entries = append(entries, XReadEntry{
-				key:   keys[i],
-				entry: streamEntries,
-			})
-		}
-	}
-
-	fmt.Println("Entries: ", entries)
-	return entries
-}
-
-func findKeyEndPosition(args []string) int {
-	for i, arg := range args {
-		if strings.Contains(arg, "-") {
-			return i
-		}
-	}
-	return 1
-}
-
-func getStreamEntriesAfterID(rdb *redisDB, key, afterID string) []redisStreamEntry {
-	var entries []redisStreamEntry
-	for _, entry := range rdb.redisStream.data[key] {
-		if entry.id > afterID {
-			entries = append(entries, entry)
-		}
-	}
-	return entries
-}
-
-func handleBlockXRead(args []string, conn net.Conn, rdb *redisDB) {
-	duration, _ := strconv.Atoi(args[0])
-	retryCount := calculateRetryCount(duration)
-
-	if duration != 0 {
-		fmt.Println("Blocking XREAD for ", duration, "ms")
-		time.Sleep(time.Millisecond * time.Duration(duration))
-	} else {
-		fmt.Println("Blocking XREAD till new data arrives")
-	}
-
-	if args[3] == "$" {
-		args[3] = lastStreamID
-	}
-
-	response := performBlockingRead(args[2:], retryCount, rdb)
-	fmt.Printf("Sent: %s\n", printCommand(response))
-	conn.Write(response)
-}
-
-func calculateRetryCount(duration int) int {
-	if duration != 0 {
-		return 1
-	}
-	return DefaultRetryCount
-}
-
-func performBlockingRead(args []string, retryCount int, rdb *redisDB) []byte {
-	for i := 0; i < retryCount; i++ {
-		entries := handleXRead(args, rdb)
-		if len(entries) > 0 {
-			return buildXReadResponse(entries)
-		}
-		if i < retryCount-1 {
-			time.Sleep(RetryInterval)
-		}
-	}
-	return []byte("$-1\r\n")
-}
-
-func buildXReadResponse(entries []XReadEntry) []byte {
-	response := fmt.Sprintf("*%d\r\n", len(entries))
-	for _, entry := range entries {
-		response += fmt.Sprintf("*2\r\n$%d\r\n%s\r\n*1\r\n", len(entry.key), entry.key)
-		for _, streamEntry := range entry.entry {
-			response += fmt.Sprintf("*2\r\n$%d\r\n%s\r\n", len(streamEntry.id), streamEntry.id)
-			for k, v := range streamEntry.fields {
-				response += fmt.Sprintf("*2\r\n$%d\r\n%s\r\n$%d\r\n%s\r\n", len(k), k, len(v), v)
-			}
-		}
-	}
-	return []byte(response)
-}
-
-func setKeyValue(key string, value any, exp int64, totalBytes int, rdb *redisDB) []byte {
-	valueType := determineValueType(value)
-	rdb.setValue(key, value, valueType, time.Now().UnixMilli(), exp)
-
-	if rdb.role == "master" {
-		rdb.offset += totalBytes
-		migrateToSlaves(key, value, rdb)
-		return encodeSimpleString("OK")
-	}
-
-	fmt.Println("Slave received set command: ", key, value, exp)
-	return []byte("")
-}
-
-func determineValueType(value any) string {
-	switch v := value.(type) {
-	case string:
-		if _, err := strconv.ParseInt(v, 10, 64); err == nil {
-			return "int"
-		}
-		return "string"
-	case int, int64, int32:
-		return "int"
-	case []string:
-		return "list"
-	case map[string]string:
-		return "hash"
-	default:
-		return "string"
-	}
 }
 
 func handleRpushCommand(args []string, totalBytes int, rdb *redisDB) []byte {
@@ -600,4 +401,20 @@ func handleLpushCommand(args []string, totalBytes int, rdb *redisDB) []byte {
 		debug("RPUSH: Key %s created with value %v\n", key, value)
 		return encodeInteger(int64(len(value)))
 	}
+}
+
+func handleLLenCommand(args []string, rdb *redisDB) []byte {
+	if len(args) != 1 {
+		return encodeError("wrong number of arguments for 'llen' command")
+	}
+
+	key := args[0]
+	val, exists := rdb.data[key]
+	if !exists {
+		return encodeInteger(0)
+	}
+
+	list := val.value.([]string)
+
+	return encodeInteger(int64(len(list)))
 }
---
+++ app/commands_utils.go
@@ -0,0 +1,210 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// Redis response encoding functions
+func encodeSimpleString(s string) []byte {
+	return []byte(fmt.Sprintf("+%s\r\n", s))
+}
+
+func encodeBulkString(s string) []byte {
+	if s == "" {
+		return []byte("$-1\r\n") // null bulk string
+	}
+	return []byte(fmt.Sprintf("$%d\r\n%s\r\n", len(s), s))
+}
+
+func encodeInteger(i int64) []byte {
+	return []byte(fmt.Sprintf(":%d\r\n", i))
+}
+
+func encodeArray(elements []string) []byte {
+	result := fmt.Sprintf("*%d\r\n", len(elements))
+	for _, element := range elements {
+		result += fmt.Sprintf("$%d\r\n%s\r\n", len(element), element)
+	}
+	return []byte(result)
+}
+
+func encodeError(msg string) []byte {
+	return []byte(fmt.Sprintf("-ERR %s\r\n", msg))
+}
+
+func encodeNull() []byte {
+	return []byte("$-1\r\n")
+}
+
+type XReadEntry struct {
+	key   string
+	entry []redisStreamEntry
+}
+
+func updateSlaveOffset(cmd string, totalBytes int, rdb *redisDB) {
+	if rdb.role == "slave" && handshakeComplete {
+		rdb.offset += totalBytes
+		fmt.Printf("\nCmd: %s,  Current Bytes: %d,  Bytes processed: %d\n", cmd, totalBytes, rdb.offset)
+	}
+}
+
+func parseStreamID(id string) (string, int) {
+	if strings.Contains(id, "-") {
+		parts := strings.Split(id, "-")
+		seq, _ := strconv.Atoi(parts[1])
+		return parts[0], seq
+	}
+	return id, 0
+}
+
+func buildStreamResponse(entries []redisStreamEntry) []byte {
+	resString := fmt.Sprintf("*%d\r\n", len(entries))
+	for _, entry := range entries {
+		resString += fmt.Sprintf("*2\r\n$%d\r\n%s\r\n", len(entry.id), entry.id)
+		for k, v := range entry.fields {
+			resString += fmt.Sprintf("*2\r\n$%d\r\n%s\r\n$%d\r\n%s\r\n", len(k), k, len(v), v)
+		}
+	}
+	return []byte(resString)
+}
+
+func handleXRead(args []string, rdb *redisDB) []XReadEntry {
+	keyEndPos := findKeyEndPosition(args)
+	keys := args[:keyEndPos]
+	ids := args[keyEndPos:]
+
+	fmt.Println("Keys: ", keys, " IDs: ", ids)
+
+	entries := make([]XReadEntry, 0)
+	for i := 0; i < len(keys); i++ {
+		streamEntries := getStreamEntriesAfterID(rdb, keys[i], ids[i])
+		if len(streamEntries) > 0 {
+			entries = append(entries, XReadEntry{
+				key:   keys[i],
+				entry: streamEntries,
+			})
+		}
+	}
+
+	fmt.Println("Entries: ", entries)
+	return entries
+}
+
+func calculateRetryCount(duration int) int {
+	if duration != 0 {
+		return 1
+	}
+	return DefaultRetryCount
+}
+
+func performBlockingRead(args []string, retryCount int, rdb *redisDB) []byte {
+	for i := 0; i < retryCount; i++ {
+		entries := handleXRead(args, rdb)
+		if len(entries) > 0 {
+			return buildXReadResponse(entries)
+		}
+		if i < retryCount-1 {
+			time.Sleep(RetryInterval)
+		}
+	}
+	return []byte("$-1\r\n")
+}
+
+func buildXReadResponse(entries []XReadEntry) []byte {
+	response := fmt.Sprintf("*%d\r\n", len(entries))
+	for _, entry := range entries {
+		response += fmt.Sprintf("*2\r\n$%d\r\n%s\r\n*1\r\n", len(entry.key), entry.key)
+		for _, streamEntry := range entry.entry {
+			response += fmt.Sprintf("*2\r\n$%d\r\n%s\r\n", len(streamEntry.id), streamEntry.id)
+			for k, v := range streamEntry.fields {
+				response += fmt.Sprintf("*2\r\n$%d\r\n%s\r\n$%d\r\n%s\r\n", len(k), k, len(v), v)
+			}
+		}
+	}
+	return []byte(response)
+}
+
+func determineValueType(value any) string {
+	switch v := value.(type) {
+	case string:
+		if _, err := strconv.ParseInt(v, 10, 64); err == nil {
+			return "int"
+		}
+		return "string"
+	case int, int64, int32:
+		return "int"
+	case []string:
+		return "list"
+	case map[string]string:
+		return "hash"
+	default:
+		return "string"
+	}
+}
+
+func findKeyEndPosition(args []string) int {
+	for i, arg := range args {
+		if strings.Contains(arg, "-") {
+			return i
+		}
+	}
+	return 1
+}
+
+func getStreamEntriesAfterID(rdb *redisDB, key, afterID string) []redisStreamEntry {
+	var entries []redisStreamEntry
+	for _, entry := range rdb.redisStream.data[key] {
+		if entry.id > afterID {
+			entries = append(entries, entry)
+		}
+	}
+	return entries
+}
+
+func validateStreamID(id string) string {
+	if id == "0-0" {
+		return "The ID specified in XADD must be greater than 0-0"
+	}
+	if id <= lastStreamID {
+		return "The ID specified in XADD is equal or smaller than the target stream top item"
+	}
+	return ""
+}
+
+func setKeyValue(key string, value any, exp int64, totalBytes int, rdb *redisDB) []byte {
+	valueType := determineValueType(value)
+	rdb.setValue(key, value, valueType, time.Now().UnixMilli(), exp)
+
+	if rdb.role == "master" {
+		rdb.offset += totalBytes
+		migrateToSlaves(key, value, rdb)
+		return encodeSimpleString("OK")
+	}
+
+	fmt.Println("Slave received set command: ", key, value, exp)
+	return []byte("")
+}
+
+func handleBlockXRead(args []string, conn net.Conn, rdb *redisDB) {
+	duration, _ := strconv.Atoi(args[0])
+	retryCount := calculateRetryCount(duration)
+
+	if duration != 0 {
+		fmt.Println("Blocking XREAD for ", duration, "ms")
+		time.Sleep(time.Millisecond * time.Duration(duration))
+	} else {
+		fmt.Println("Blocking XREAD till new data arrives")
+	}
+
+	if args[3] == "$" {
+		args[3] = lastStreamID
+	}
+
+	response := performBlockingRead(args[2:], retryCount, rdb)
+	fmt.Printf("Sent: %s\n", printCommand(response))
+	conn.Write(response)
+}
