7ab0a823fa05679f8c06f26a124ae1837200dca4
LitoleNINJA <sritwik2@gmail.com>
2025-10-21 01:04:06 +0530

    codecrafters submit [skip ci]

--- app/commands_main.go
+++ app/commands_main.go
@@ -766,7 +766,7 @@ func handleGeoposCommnad(args []string, rdb *redisDB) []byte {
 	for i:=1; i<len(args); i++ {
 		score, found := ss.getScore(args[i])
 		if !found {
-			resp = append(resp, encodeArray([]any{}))
+			resp = append(resp, "*-1\r\n")
 			continue
 		}
 	
